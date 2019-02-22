30e2274405ce7f84c4acc11227dcc90bf08e5b0c
Victt0r <dsnobik@gmail.com>
2019-02-22 12:02:09 +0200

    table building todo 0.1

--- todo.js
+++ todo.js
@@ -4,8 +4,44 @@ request('GET', url, handler, console.log);
 function handler(response) {
   if (response.startsWith("[")) {
     response = JSON.parse(response);
+    console.log(response);
     // setCookie('user_id', getCookie('user_id'), {expires:60*60*24*30});
     // setCookie('token', response.token, {expires:60*60*24*30});
-    console.log(response);
+
+    var table = document.createElement('table');
+    section.append(table);
+    table.innerHTML = `<tr>
+    <th></th>
+    <th>ВВС</th>
+    <th>действие</th>
+    <th>сделано (всего)</th>
+    <th>залог/награда</th>
+  </tr>`
+    response.forEach(buildRow);
+    function buildRow(row) {
+      var tr = document.createElement('tr');
+      //  checkBox
+      var td = document.createElement('td');
+      var input = document.createElement('input');
+      table.append(tr);
+      tr.append(td);
+      td.append(input);
+      input.setAttribute('type', 'checkbox');
+      td = document.createElement('td');
+      td.innerText = '+'+row[2];
+      tr.append(td);
+      td = document.createElement('td');
+      td.innerText = row[6];
+      tr.append(td);
+      td = document.createElement('td');
+      td.innerText = row[7]-row[8]+'(из '+row[7]+'дней)';
+      tr.append(td);
+      td = document.createElement('td');
+      var pledge = row[9]/row[7]*(row[7]-row[10]);
+      td.innerText = pledge+(pledge==row[9]?'': '/'+row[9]);
+      tr.append(td);
+
+
+    }
   }
 }
